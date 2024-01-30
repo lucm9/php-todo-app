cb59e7ab7158c7594bbdd3de52990e0010991608
Cloud User <ec2-user@ip-172-31-8-164.us-west-1.compute.internal>
2024-01-30 06:12:57 +0000

    testphpunit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+// File: /home/ec2-user/php-todo-app/tests/ExampleTest.php
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
